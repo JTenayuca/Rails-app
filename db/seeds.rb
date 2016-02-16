b3d0cf9991ead5b563babe339ddec6744ff58f4e
John Tenayuca <JTenayuca@gmail.com>
2016-02-16 17:07:37 +0000

    Products db update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(:name => "Seed generated product", :description => "I also created this product without using the html form!")
